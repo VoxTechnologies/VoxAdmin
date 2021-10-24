c3c9326468eadf1a0abb1ac9d89726db603ece49
Yoichiro Hara <hara@hacci.net>
2021-10-24 22:23:36 +0900

    d

--- dist/assets/js/custom/widgets.js
+++ dist/assets/js/custom/widgets.js
@@ -2,6 +2,126 @@
 
 // Class definition
 var KTWidgets = function () {
+
+    // Column Chart
+    var element = document.getElementById('kt_apexcharts_1');
+
+    var height = parseInt(KTUtil.css(element, 'height'));
+    var labelColor = KTUtil.getCssVariableValue('--bs-gray-500');
+    var borderColor = KTUtil.getCssVariableValue('--bs-gray-200');
+    var baseColor = KTUtil.getCssVariableValue('--bs-primary');
+    var secondaryColor = KTUtil.getCssVariableValue('--bs-gray-300');
+
+    if (!element) {
+        return;
+    }
+
+    var options = {
+        series: [{
+            name: 'Gross Margin',
+            data: [44, 55, 57, 56, 61, 58]
+        }, {
+            name: 'Sales Transaction',
+            data: [76, 85, 101, 98, 87, 105]
+        }],
+        chart: {
+            fontFamily: 'inherit',
+            type: 'bar',
+            height: height,
+            toolbar: {
+                show: false
+            }
+        },
+        plotOptions: {
+            bar: {
+                horizontal: false,
+                columnWidth: ['30%'],
+                endingShape: 'rounded'
+            },
+        },
+        legend: {
+            show: false
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            show: true,
+            width: 2,
+            colors: ['transparent']
+        },
+        xaxis: {
+            categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+            axisBorder: {
+                show: false,
+            },
+            axisTicks: {
+                show: false
+            },
+            labels: {
+                style: {
+                    colors: labelColor,
+                    fontSize: '12px'
+                }
+            }
+        },
+        yaxis: {
+            labels: {
+                style: {
+                    colors: labelColor,
+                    fontSize: '12px'
+                }
+            }
+        },
+        fill: {
+            opacity: 1
+        },
+        states: {
+            normal: {
+                filter: {
+                    type: 'none',
+                    value: 0
+                }
+            },
+            hover: {
+                filter: {
+                    type: 'none',
+                    value: 0
+                }
+            },
+            active: {
+                allowMultipleDataPointsSelection: false,
+                filter: {
+                    type: 'none',
+                    value: 0
+                }
+            }
+        },
+        tooltip: {
+            style: {
+                fontSize: '12px'
+            },
+            y: {
+                formatter: function (val) {
+                    return '$' + val + ' thousands'
+                }
+            }
+        },
+        colors: [baseColor, secondaryColor],
+        grid: {
+            borderColor: borderColor,
+            strokeDashArray: 4,
+            yaxis: {
+                lines: {
+                    show: true
+                }
+            }
+        }
+    };
+
+    var chart = new ApexCharts(element, options);
+    chart.render();
+
     // Charts widgets
     var initChartsWidget1 = function() {
         var element = document.getElementById("kt_charts_widget_1_chart");
