6a2e7467a0fbb29a22b2235b2ff617cd35f8e1ba
Yoichiro Hara <hara@hacci.net>
2021-11-05 01:14:53 +0900

    s

--- dist/assets/plugins/global/plugins.bundle.js
+++ dist/assets/plugins/global/plugins.bundle.js
@@ -73706,3 +73706,19 @@ swal.mixin({
 	cancelButtonClass: 'btn btn-secondary',
 	cancelButtonColor: null
 });
+
+// Dropzone
+var myDropzone = new Dropzone("#kt_dropzonejs_example_1", {
+	url: "https://keenthemes.com/scripts/void.php", // Set the url for your upload script location
+	paramName: "file", // The name that will be used to transfer the file
+	maxFiles: 10,
+	maxFilesize: 10, // MB
+	addRemoveLinks: true,
+	accept: function(file, done) {
+		if (file.name == "wow.jpg") {
+			done("Naha, you don't.");
+		} else {
+			done();
+		}
+	}
+});
